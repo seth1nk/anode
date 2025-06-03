b6a536078655581269e3ccc427e165787e2af610
seth1nk <kostik.vs10@mail.ru>
2025-06-03 12:48:48 +0800

    Update createAdmin.js

--- fills/createAdmin.js
+++ fills/createAdmin.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 // Подключение к PostgreSQL (Clever Cloud)
-const sequelize = new Sequelize('postgresql://uxlnpg4ykz3j9bcpxmiy:JvSeEcRrqQL3iYSG59FJaR0QQusd8e@bsuedgclroq7zxra2qkr-postgresql.services.clever-cloud.com:50013/bsuedgclroq7zxra2qkr', {
+const sequelize = new Sequelize('postgresql://ujjqvrrnvzdiuiuodt3z:8MLdU9s1BxciYFb0DxkbnlHmGFV7sW@bcvucojp6i3gosgzcvqr-postgresql.services.clever-cloud.com:50013/bcvucojp6i3gosgzcvqr', {
     dialect: 'postgres',
     logging: console.log,
     dialectOptions: {
